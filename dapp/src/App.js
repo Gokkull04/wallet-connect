70d520430e94681887709855cba569b79d8bd707
Gokkull04 <gokkull04@gmail.com>
2024-08-21 22:55:55 +0530

    wallet connected

--- dapp/src/App.js
+++ dapp/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Button from './button';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      
+    <div className="min-h-screen flex flex-col">
+      <header className="bg-gray-800 text-white py-4 shadow-lg">
+        <div className="container mx-auto flex justify-between items-center px-4">
+          <div className="text-xl font-bold">wallet-connect</div>
+            <Button />
+          </div>
+      </header>
+      <main className="flex-grow flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-gray-700">Welcome to Wallet Connect</h1>
+      </main>
     </div>
-  )
+  );
 }
+
+export default App;
